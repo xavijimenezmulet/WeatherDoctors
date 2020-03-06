838e1dfefa680347d1ceb1f211caf58cbcb8bb5b
xavijimenezmulet <xavijimenezmulet@users.noreply.github.com>
2020-03-06 06:53:20 +0100

    Adding splash activity, lottie implementation and create fade in and fade out transition

--- app/src/main/java/com/xjm/weatherdoctors/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/xjm/weatherdoctors/presentation/splash/SplashActivity.kt
@@ -1,13 +1,50 @@
 package com.xjm.weatherdoctors.presentation.splash
 
+import android.animation.Animator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.airbnb.lottie.LottieAnimationView
 import com.xjm.weatherdoctors.R
+import com.xjm.weatherdoctors.presentation.dashboard.DashboardActivity
+
 
 class SplashActivity : AppCompatActivity() {
+
+    var splashLottie: LottieAnimationView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        bindViews()
+        configAnimation()
+    }
+
+    /**
+     * Called to bind views
+     */
+    private fun bindViews() {
+        splashLottie = findViewById(R.id.splash_lottie)
+    }
+
+    private fun configAnimation() {
+        splashLottie?.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(animation: Animator?) {
+                //Nothing to do
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                startActivity(Intent(this@SplashActivity, DashboardActivity::class.java))
+                overridePendingTransition(R.xml.fade_in, R.xml.fade_out)
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+                //Nothing to do
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+                //Nothing to do
+            }
+        })
     }
 }
